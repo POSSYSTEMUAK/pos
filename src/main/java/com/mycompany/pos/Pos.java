d2af4f3f6a165b804dd44197bdedb46b462e0b81
AliAhsan69 <102369293+AliAhsan69@users.noreply.github.com>
2024-11-07 16:41:31 +0500

    1st commit

---
+++ src/main/java/com/mycompany/pos/Pos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pos;
+
+/**
+ *
+ * @author talat
+ */
+public class Pos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
