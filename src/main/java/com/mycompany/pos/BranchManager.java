890aa0d69a97b31d42cef914a00e789c4e4e30ad
AliAhsan69 <l215827@lhr.nu.edu.pk>
2024-11-09 23:47:58 +0500

    SuperAdmin and BranchManager added

--- src/main/java/com/mycompany/pos/BranchManager.java
+++ src/main/java/com/mycompany/pos/BranchManager.java
@@ -1,4 +1,41 @@
 package com.mycompany.pos;
 
-public class BranchManager {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class BranchManager extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Branch Manager Dashboard");
+
+        Label header = new Label("Branch Manager Dashboard");
+
+        // Employee Management Section
+        Label employeeLabel = new Label("Employee Management");
+        TextField employeeName = new TextField();
+        employeeName.setPromptText("Employee Name");
+        TextField employeeRole = new TextField();
+        employeeRole.setPromptText("Role (Cashier/Data Entry Operator)");
+
+        Button addEmployeeButton = new Button("Add Employee");
+        Button resetPasswordButton = new Button("Reset Employee Password");
+
+        VBox layout = new VBox(10, header, employeeLabel, employeeName, employeeRole,
+                addEmployeeButton, resetPasswordButton);
+
+        Scene scene = new Scene(layout, 300, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
